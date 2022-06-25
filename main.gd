7be0c63dba6aeb97b9c3694133977761820d8d4a
matheusfillipe <matheusfillipeag@gmail.com>
2022-06-25 01:05:22 -0300

    Commands refactored

--- main.gd
+++ main.gd
@@ -18,6 +18,32 @@ onready var scroll_container = $ScrollContainer
 onready var label = $ScrollContainer/Label
 onready var text_edit = $TextEdit
 
+var command_prefix = "/"
+
+enum Commands {
+	HELP
+	CLEAR
+	ME
+	PART
+	NICK
+	JOIN
+	TOPIC
+	MSG
+	QUIT
+}
+
+const CMD_HELP = {
+	Commands.HELP	: "Usage: /help command",
+	Commands.CLEAR	: "Clears the screen",
+	Commands.ME		: "Sends a message as an action. Usage: /me message",
+	Commands.PART	: "Usage: /part <channel>",
+	Commands.NICK	: "Usage: /nick <new nickname.",
+	Commands.JOIN	: "Usage: /join <channel>",
+	Commands.TOPIC	: "Usage: /topic topic",
+	Commands.MSG		: "Usage: /msg <nick> message",
+	Commands.QUIT	: "Usage: /quit message",
+}
+
 var client: IrcClient
 
 func _ready():
@@ -46,7 +72,6 @@ func _on_event(ev):
 	match ev.type:
 		client.PRIVMSG:
 			label.text += ev.channel + " -> " + ev.nick + ": " + ev.message + "\n"
-			scrolldown()
 		client.PART:
 			label.text += "LEFT " + ev.channel + "\n"
 		client.JOIN:
@@ -54,59 +79,91 @@ func _on_event(ev):
 		client.ACTION:
 			label.text += ev.channel + " -> " + ev.nick + ": " + "*" + ev.message + "*\n"
 		client.NAMES:
-			pass
+			label.text += "Users in channel: " + str(ev.names) + "\n"
 		client.NICK:
-			pass
+			label.text += "You are now known as " + ev.nick + "\n"
 		client.NICK_IN_USE:
-			pass
+			label.text += "That nickname is already in use!\n"
+
+	scrolldown()
 
 func _input(ev):
 		if ev.is_action_pressed("send"):
 			_on_Send_pressed()
 
+static func join_from(args, start_index=0):
+	var string = ""
+	var i = -1
+
+	for word in args:
+		i += 1
+		if i < start_index:
+			continue
+		string += word + " "
+	return string
+
+func _command(text):
+	var whitespace_split = text.split(" ")
+	var command = whitespace_split[0].trim_prefix(command_prefix)
+	var args = PoolStringArray()
+	if len(whitespace_split) > 1:
+		args = text.trim_prefix(command_prefix + command + " ").split(" ")
+	command = command.to_upper()
+
+	match Commands.keys().find(command):
+		Commands.HELP:
+			if len(args) > 0:
+				var cmd = args[0].to_upper()
+				if not cmd in Commands.keys():
+					label.text += "Unrecognized command: /" + args[0] + "\n"
+					return
+				var help_msg = CMD_HELP[Commands.keys().find(cmd)]
+				label.text += "/" + cmd + ": " + help_msg + "\n"
+				return
+
+			for cmd in Commands.keys():
+				label.text += command_prefix + cmd + "\n"
+			label.text += "\n"
+
+		Commands.CLEAR:
+			label.text = ""
+		Commands.ME:
+			client.me(channel, join_from(args))
+		Commands.PART:
+			client.part(channel)
+		Commands.NICK:
+			client.set_nick(args[0])
+			nick = args[0]
+		Commands.JOIN:
+			client.join(args[0])
+			channel = args[0]
+		Commands.TOPIC:
+			client.topic(channel, join_from(args))
+		Commands.MSG:
+			client.send(args[0], join_from(args, 1))
+		Commands.QUIT:
+			client.quit(join_from(args))
+		_:
+			label.text += "Unrecognized command: /" + command + "\n"
+
+
 func _on_Send_pressed():
 	for text in text_edit.text.split("\n"):
-		if len(text) > 0:
-			if text.begins_with('/'):
-				
-				var capscommand = text.split(' ')[0].to_upper().trim_prefix('/')
-				var command = text.split(' ')[0]
-				var args = get_args(text.split(' ')[0], text)
-				var joinedargs = join_args(args)
-				print(command)
-				match capscommand:
-					"CLEAR":
-						label.text = ''
-					"ME":
-						client.me(channel, joinedargs)
-					"PART":
-						client.part(channel)
-					"NICK":
-						client.set_nick(args[0])
-					"JOIN":
-						client.join(args[0])
-					"TOPIC":
-						client.topic(channel,joinedargs)
-					"QUIT":
-						client.quit(joinedargs)
-				text_edit.text = ""
-				return
-			client.send(channel, text)
-			label.text += channel + " -> " + nick + ": " + text + "\n"
+		if len(text) <= 0:
+			continue
+
+		# If is command
+		if text.begins_with(command_prefix):
+			_command(text)
+			continue
+
+		# Send message to current channel
+		client.send(channel, text)
+		label.text += channel + " -> " + nick + ": " + text + "\n"
+
 	text_edit.text = ""
 	scrolldown()
-func get_args(command, string):
-	var newstring = string.trim_prefix(command)
-	print(newstring)
-	var array = []
-	for args in newstring.lstrip(newstring.split(' ')[0]+' '):
-		array.append(args)
-	return array
-func join_args(args):
-	var string = ''
-	for i in args:
-		string += i
-	return string
+
 func scrolldown():
 	var bar: VScrollBar = scroll_container.get_v_scrollbar();
 	scroll_container.scroll_vertical = bar.max_value;
