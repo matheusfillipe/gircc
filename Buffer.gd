caf22f261c01c96d7cf5a0e609dd65d15ce2d33d
matheusfillipe <matheusfillipeag@gmail.com>
2022-09-04 01:59:36 -0300

    wip font support

--- Buffer.gd
+++ Buffer.gd
@@ -3,11 +3,42 @@ var channel
 onready var scroll = $ScrollContainer/VBoxContainer
 
 const COLORS = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#00ffff", "#ff00ff"]
+const ColorEscape = "\u0003";
+const BoldEscape = "";
+const ItalicEscape = "";
+const UnderlineEscape = "";
 
 
 # TODO parse irc colors, special texts
 func _parse_irc_text(text):
-	var parsed = text
+	var parsed = ""
+	var italic = false
+	var bold = false
+	var underline = false
+	for c in text:
+		if c == ColorEscape:
+			pass
+		elif c == BoldEscape:
+			if bold:
+				parsed += "[/b]"
+			else:
+				parsed += "[b]"
+			bold = !bold
+		elif c == ItalicEscape:
+			if italic:
+				parsed += "[/i]"
+			else:
+				parsed += "[i]"
+			italic = !italic
+		elif c == UnderlineEscape:
+			if underline:
+				parsed += "[/u]"
+			else:
+				parsed += "[u]"
+			underline = !underline
+		else:
+			parsed += c
+
 	return parsed
 
 func add_message(text, nick=null, color=null):
